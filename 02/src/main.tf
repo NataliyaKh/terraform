34c4b0052ccff68bb2aad2171d35b21265576d44
NataliyaKh <wastelin@mail.ru>
2025-04-01 23:52:13 +0300

    files

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -14,11 +14,11 @@ data "yandex_compute_image" "ubuntu" {
 }
 resource "yandex_compute_instance" "platform" {
   name        = "netology-develop-platform-web"
-  platform_id = "standart-v4"
+  platform_id = "standard-v3"
   resources {
-    cores         = 1
+    cores         = 2
     memory        = 1
-    core_fraction = 5
+    core_fraction = 20
   }
   boot_disk {
     initialize_params {
@@ -35,7 +35,9 @@ resource "yandex_compute_instance" "platform" {
 
   metadata = {
     serial-port-enable = 1
-    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+    #ssh-keygen -t ed25519  Забудьте уже про rsa ключи!!
+    # ubuntu - дефолтный пользователь в ubuntu :)
+    ssh-keys = "ubuntu:${var.vms_ssh_root_key}"
   }
 
 }
